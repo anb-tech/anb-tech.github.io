dca26e10e0907d97aa29b00d59a137f7ce9bd67d
alexandr_belyaev <anbelyaevbit@gmail.com>
2021-01-02 23:31:53 +0300

    update js

--- beacon_controler/js/main.js
+++ beacon_controler/js/main.js
@@ -16,8 +16,8 @@ function requestGET () {
 	request.addEventListener('load', function() {
 		if (request.status == 200) {
 			data = JSON.parse(request.response);
-			createPoint(data);
 			console.log(data);
+			createPoint(data);
 			numberPoints = data.length;
 
 		} else {
@@ -63,97 +63,97 @@ let onPathClick = (e) => {
 	console.log(e.currentTarget);
 
 ////////////////////////// elem motion //////////////////////////
-	if (target.matches('.motion__elem')) {
+if (target.matches('.motion__elem')) {
 
-		target.addEventListener('mousedown', function(e){
+	target.addEventListener('mousedown', function(e){
+		e.preventDefault();
+		var startCoords = {
+			x: e.clientX,
+			y: e.clientY
+		};
+
+		var onMouseMove	= function(e) {
 			e.preventDefault();
-			var startCoords = {
+
+			var shift = {
+				x: startCoords.x - e.clientX,
+				y: startCoords.y - e.clientY
+			};
+
+			startCoords	= {
 				x: e.clientX,
 				y: e.clientY
 			};
 
-			var onMouseMove	= function(e) {
-				e.preventDefault();
-
-				var shift = {
-					x: startCoords.x - e.clientX,
-					y: startCoords.y - e.clientY
-				};
-
-				startCoords	= {
-					x: e.clientX,
-					y: e.clientY
-				};
-
-				target.style.top = (target.offsetTop - shift.y) + 'px';
-				target.style.left = (target.offsetLeft - shift.x) + 'px';
-			};
-
-			var onMouseUp = function(e) {
-				e.preventDefault();
-				document.removeEventListener('mousemove', onMouseMove);
-				document.removeEventListener('mouseup', onMouseUp)
-			}
-
-			document.addEventListener('mousemove', onMouseMove);
-			document.addEventListener('mouseup', onMouseUp);
-		});
-
-	};
-
-////////////////////////// show menu //////////////////////////
-	if (target.matches('.point')) {
-		target.addEventListener('contextmenu', function(e) {
-			if (formWrapper.classList.contains('hidden')){
-				formWrapper.classList.remove('hidden');
-			}
-			else {
-				formWrapper.classList.add('hidden');
-			}
-		});
-	};
-
-////////////////////////// add elem on path //////////////////////////
-	if (target == motionPath) {
-
-		let newPoint = similarPointTemplate.cloneNode(true);
-		numberPoints++;
-		console.log(numberPoints);
-
-		newPoint.querySelector('.point__name').textContent = numberPoints;
-		newPoint.querySelector('.point__radius2').style.width = data[data.length-1].radius2  + 'px';
-		newPoint.querySelector('.point__radius2').style.height = data[data.length-1].radius2  + 'px';
-		newPoint.querySelector('.point__radius1').style.width = data[data.length-1].radius1 + 'px';
-		newPoint.querySelector('.point__radius1').style.height = data[data.length-1].radius1 + 'px';
-		newPoint.style.top = e.clientY - (data[data.length-1].radius2)/2 + 'px';
-		newPoint.style.left = e.clientX - (data[data.length-1].radius2)/2 + 'px';
-
-
-		motionPath.appendChild(newPoint);
-
-		function requestPOST (e) {
-
-			let obj = {
-				name: numberPoints,
-				radius1: data[data.length-1].radius1,
-				radius2: data[data.length-1].radius2,
-				signal_quality1: 80,
-				signal_quality2: 50,
-				top_position: e.clientY - (data[data.length-1].radius2)/2,
-				left_position: e.clientX - (data[data.length-1].radius2)/2,
-				id: numberPoints
-			};
-
-			let json = JSON.stringify(obj);
-
-			const request = new XMLHttpRequest();
-			request.open('POST', 'https://anb-tech.github.io/all_data/beacons_controler.json');
-			request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-			request.send(json);
+			target.style.top = (target.offsetTop - shift.y) + 'px';
+			target.style.left = (target.offsetLeft - shift.x) + 'px';
 		};
 
-		requestPOST(e);
+		var onMouseUp = function(e) {
+			e.preventDefault();
+			document.removeEventListener('mousemove', onMouseMove);
+			document.removeEventListener('mouseup', onMouseUp)
+		}
+
+		document.addEventListener('mousemove', onMouseMove);
+		document.addEventListener('mouseup', onMouseUp);
+	});
+
+};
+
+////////////////////////// show menu //////////////////////////
+if (target.matches('.point')) {
+	target.addEventListener('contextmenu', function(e) {
+		if (formWrapper.classList.contains('hidden')){
+			formWrapper.classList.remove('hidden');
+		}
+		else {
+			formWrapper.classList.add('hidden');
+		}
+	});
+};
+
+////////////////////////// add elem on path //////////////////////////
+if (target == motionPath) {
+
+	let newPoint = similarPointTemplate.cloneNode(true);
+	numberPoints++;
+	console.log(numberPoints);
+
+	newPoint.querySelector('.point__name').textContent = numberPoints;
+	newPoint.querySelector('.point__radius2').style.width = data[data.length-1].radius2  + 'px';
+	newPoint.querySelector('.point__radius2').style.height = data[data.length-1].radius2  + 'px';
+	newPoint.querySelector('.point__radius1').style.width = data[data.length-1].radius1 + 'px';
+	newPoint.querySelector('.point__radius1').style.height = data[data.length-1].radius1 + 'px';
+	newPoint.style.top = e.clientY - (data[data.length-1].radius2)/2 + 'px';
+	newPoint.style.left = e.clientX - (data[data.length-1].radius2)/2 + 'px';
+
+
+	motionPath.appendChild(newPoint);
+
+	function requestPOST (e) {
+
+		let obj = {
+			name: numberPoints,
+			radius1: data[data.length-1].radius1,
+			radius2: data[data.length-1].radius2,
+			signal_quality1: 80,
+			signal_quality2: 50,
+			top_position: e.clientY - (data[data.length-1].radius2)/2,
+			left_position: e.clientX - (data[data.length-1].radius2)/2,
+			id: numberPoints
+		};
+
+		let json = JSON.stringify(obj);
+
+		const request = new XMLHttpRequest();
+		request.open('POST', 'https://anb-tech.github.io/all_data/beacons_controler.json');
+		request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+		request.send(json);
 	};
+
+	requestPOST(e);
+};
 };
 
 motionPath.addEventListener('click',onPathClick);
