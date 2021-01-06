1f5424b87165abf4f2ff5674d87b8af8a88d3881
alexandr_belyaev <anbelyaevbit@gmail.com>
2021-01-06 22:06:52 +0300

    upgrate requests to localStorage

--- beacon_controler/js/main.js
+++ beacon_controler/js/main.js
@@ -1,3 +1,28 @@
+let initialData = {
+    "points": [
+        {
+            "name": 1,
+            "radius1": 60,
+            "radius2": 100,
+            "signal_quality1": 50,
+            "signal_quality2": 20,
+            "top_position": 200,
+            "left_position": 200,
+            "id": 1
+        },
+        {
+            "name": 2,
+            "radius1": 50,
+            "radius2": 80,
+            "signal_quality1": 80,
+            "signal_quality2": 50,
+            "top_position": 300,
+            "left_position": 350,
+            "id": 2
+        }
+    ]
+}
+
 let motionPath = document.querySelector('.motion__path');
 let similarPointTemplate = document.querySelector('#similar-point-template').content.querySelector('.point');
 let motionElemList = document.querySelectorAll('.motion__elem');
@@ -8,32 +33,39 @@ var data = [];
 var numberPoints;
 
 ////////////////////////// work with data //////////////////////////
-function requestGET () {
-	const request = new XMLHttpRequest();
-	request.open('GET', 'https://anb-tech.github.io/all_data/beacons_controler.json');
-	request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-	request.send();
-	request.addEventListener('load', function() {
-		if (request.status == 200) {
-			data = JSON.parse(request.response).points;
-			console.log(data);
-			createPoint(data);
-			numberPoints = data.length;
 
-		} else {
-			console.error('Ошибка');
-		}
-	});
-}
+
+function requestGET() {
+    data = JSON.parse(localStorage.getItem('data')).points;
+    numberPoints = data.length;
+    createPoint(data);
+
+
+    /* const request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:3000/points');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+    request.addEventListener('load', function () {
+        if (request.status == 200) {
+            data = JSON.parse(request.response);
+            createPoint(data);
+            console.log(data);
+            numberPoints = data.length;
+
+        } else {
+            console.error('Ошибка');
+        }
+    }); */
+};
 
 function createPoint(data) {
-	data.forEach(item => {
-		let point = document.createElement('div');
-		point.classList.add('point', 'motion__elem');
-		point.style.top = (item.top_position) + 'px';
-		point.style.left = (item.left_position) + 'px';
+    data.forEach(item => {
+        let point = document.createElement('div');
+        point.classList.add('point', 'motion__elem');
+        point.style.top = (item.top_position) + 'px';
+        point.style.left = (item.left_position) + 'px';
 
-		point.innerHTML = `
+        point.innerHTML = `
 		<div class="point__inner">
 		<div class="point__radius2" style="width: ${item.radius2}px; height: ${item.radius2}px">
 		<div class="point__radius1" style="width: ${item.radius1}px; height: ${item.radius1}px">
@@ -45,117 +77,124 @@ function createPoint(data) {
 		${item.name}
 		</p>
 		`;
-		motionPath.appendChild(point);
-	});
+        motionPath.appendChild(point);
+    });
 };
 
-
 window.addEventListener('DOMContentLoaded', () => {
-	requestGET();
-	console.log(data);
+    localStorage.setItem('data', JSON.stringify(initialData));
+    requestGET();
 });
 
 let onPathClick = (e) => {
 
-	const target = e.target;
-
-	console.log(target);
-	console.log(e.currentTarget);
+    const target = e.target;
+/*
+    console.log(target);
+    console.log(e.currentTarget);
+*/
 
 ////////////////////////// elem motion //////////////////////////
-if (target.matches('.motion__elem')) {
 
-	target.addEventListener('mousedown', function(e){
-		e.preventDefault();
-		var startCoords = {
-			x: e.clientX,
-			y: e.clientY
-		};
+    if (target.matches('.motion__elem')) {
 
-		var onMouseMove	= function(e) {
-			e.preventDefault();
+        target.addEventListener('mousedown', function (e) {
+            e.preventDefault();
+            var startCoords = {
+                x: e.clientX,
+                y: e.clientY
+            };
 
-			var shift = {
-				x: startCoords.x - e.clientX,
-				y: startCoords.y - e.clientY
-			};
+            var onMouseMove = function (e) {
+                e.preventDefault();
 
-			startCoords	= {
-				x: e.clientX,
-				y: e.clientY
-			};
+                var shift = {
+                    x: startCoords.x - e.clientX,
+                    y: startCoords.y - e.clientY
+                };
 
-			target.style.top = (target.offsetTop - shift.y) + 'px';
-			target.style.left = (target.offsetLeft - shift.x) + 'px';
-		};
+                startCoords = {
+                    x: e.clientX,
+                    y: e.clientY
+                };
 
-		var onMouseUp = function(e) {
-			e.preventDefault();
-			document.removeEventListener('mousemove', onMouseMove);
-			document.removeEventListener('mouseup', onMouseUp)
-		}
+                target.style.top = (target.offsetTop - shift.y) + 'px';
+                target.style.left = (target.offsetLeft - shift.x) + 'px';
+            };
 
-		document.addEventListener('mousemove', onMouseMove);
-		document.addEventListener('mouseup', onMouseUp);
-	});
+            var onMouseUp = function (e) {
+                e.preventDefault();
+                document.removeEventListener('mousemove', onMouseMove);
+                document.removeEventListener('mouseup', onMouseUp)
+            }
 
-};
+            document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('mouseup', onMouseUp);
+        });
+
+    };
 
 ////////////////////////// show menu //////////////////////////
-if (target.matches('.point')) {
-	target.addEventListener('contextmenu', function(e) {
-		if (formWrapper.classList.contains('hidden')){
-			formWrapper.classList.remove('hidden');
-		}
-		else {
-			formWrapper.classList.add('hidden');
-		}
-	});
-};
+
+    if (target.matches('.point')) {
+        target.addEventListener('contextmenu', function (e) {
+            if (formWrapper.classList.contains('hidden')) {
+                formWrapper.classList.remove('hidden');
+            } else {
+                formWrapper.classList.add('hidden');
+            }
+        });
+    };
 
 ////////////////////////// add elem on path //////////////////////////
-if (target == motionPath) {
 
-	let newPoint = similarPointTemplate.cloneNode(true);
-	numberPoints++;
-	console.log(numberPoints);
+    if (target == motionPath) {
 
-	newPoint.querySelector('.point__name').textContent = numberPoints;
-	newPoint.querySelector('.point__radius2').style.width = data[data.length-1].radius2  + 'px';
-	newPoint.querySelector('.point__radius2').style.height = data[data.length-1].radius2  + 'px';
-	newPoint.querySelector('.point__radius1').style.width = data[data.length-1].radius1 + 'px';
-	newPoint.querySelector('.point__radius1').style.height = data[data.length-1].radius1 + 'px';
-	newPoint.style.top = e.clientY - (data[data.length-1].radius2)/2 + 'px';
-	newPoint.style.left = e.clientX - (data[data.length-1].radius2)/2 + 'px';
+        let newPoint = similarPointTemplate.cloneNode(true);
+        numberPoints++;
+
+        newPoint.querySelector('.point__name').textContent = numberPoints;
+        newPoint.querySelector('.point__radius2').style.width = data[data.length - 1].radius2 + 'px';
+        newPoint.querySelector('.point__radius2').style.height = data[data.length - 1].radius2 + 'px';
+        newPoint.querySelector('.point__radius1').style.width = data[data.length - 1].radius1 + 'px';
+        newPoint.querySelector('.point__radius1').style.height = data[data.length - 1].radius1 + 'px';
+        newPoint.style.top = e.clientY - (data[data.length - 1].radius2) / 2 + 'px';
+        newPoint.style.left = e.clientX - (data[data.length - 1].radius2) / 2 + 'px';
 
 
-	motionPath.appendChild(newPoint);
+        motionPath.appendChild(newPoint);
 
-	function requestPOST (e) {
+        function requestPOST(e) {
 
-		let obj = {
-			name: numberPoints,
-			radius1: data[data.length-1].radius1,
-			radius2: data[data.length-1].radius2,
-			signal_quality1: 80,
-			signal_quality2: 50,
-			top_position: e.clientY - (data[data.length-1].radius2)/2,
-			left_position: e.clientX - (data[data.length-1].radius2)/2,
-			id: numberPoints
-		};
+            let obj = {
+                name: numberPoints,
+                radius1: data[data.length - 1].radius1,
+                radius2: data[data.length - 1].radius2,
+                signal_quality1: 80,
+                signal_quality2: 50,
+                top_position: e.clientY - (data[data.length - 1].radius2) / 2,
+                left_position: e.clientX - (data[data.length - 1].radius2) / 2,
+                id: numberPoints
+            };
 
-		let json = JSON.stringify(obj);
+            const points = JSON.parse(localStorage.getItem('data')).points;
+            const dataCome = JSON.parse(localStorage.getItem('data'));
+            points.push(obj);
+            let request = JSON.stringify(
+                {
+                    points: points
+                }
+            );
+            localStorage.setItem('data',request);
 
-		const request = new XMLHttpRequest();
-		request.open('POST', 'https://anb-tech.github.io/all_data/beacons_controler.json');
-		request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-		request.send(json);
-	};
-
-	requestPOST(e);
+ 
+            /* const request = new XMLHttpRequest();
+            request.open('POST', 'http://localhost:3000/points');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send(json); */
+        };
+        requestPOST(e);
+    };
 };
-};
 
-motionPath.addEventListener('click',onPathClick);
-
-
+motionPath.addEventListener('click', onPathClick);
